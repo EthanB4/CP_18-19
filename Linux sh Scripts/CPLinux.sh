b6fd2af56011311eb158de597a52747de9209232
Ethan <43523003+EthanB4@users.noreply.github.com>
2018-10-29 14:24:41 -0500

    Add files via upload

---
+++ Linux sh Scripts/CPLinux.sh
@@ -0,0 +1,80 @@
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+GREY='\033[0;37m'
+RED='\033[0;31m'
+DGREY='\033[1;30m'
+BLANK='\033[0m'
+printf "$YELLOW"
+echo "                           /\                              "
+echo "                          /  \                             "
+echo "                         /    \                            " 
+echo "                        /      \                           " 
+echo "                       /        \                          " 
+echo "                      /          \                         "
+echo "                     /            \                        "
+echo "                    /______________\                       "
+echo "                   / \            / \                      "
+echo "                  /   \          /   \                     "
+echo "                 /     \        /     \                    "
+echo "                /       \      /       \                   "
+echo "               /         \    /         \                  "
+echo "              /           \  /           \                 "
+echo "             /             \/             \                "
+echo "            /______________________________\               "
+printf "$CYAN"
+echo "                CyberPatriot 2018-2019                     "
+printf "$BLANK" 
+echo ""
+echo ""
+echo ""
+sudo apt-get install ufw -y
+sudo ufw enable
+sudo apt-get install firefox -y
+sudo wget https://github.com/CISOfy/lynis
+sleep 2.5
+sudo apt-get install lynis -y
+sudo gtk-launch lynis.desktop
+wget https://github.com/GNOME/gimp
+sudo apt-get install Gimp
+sudo xdg-open /etc/lightdm/lightdm.conf
+User=`id -un`
+
+
+
+printf "$RED"
+echo "             ___     ____         ___      " 
+echo "            /   |   /    \       /   |     " 
+echo "           / /| |  |  /\  |     / /| |     " 
+echo "          / / | |  | |  | |    / / | |     " 
+echo "         / /  | |  | |  | |   / /  | |     " 
+echo "        / /___| |  | |  | |  / /___| |     " 
+echo "       /______| |  | |  | | /______| |     " 
+echo "              | |  |  \/  |        | |     " 
+echo "              |_|   \____/         |_|     " 
+printf "$GREY"
+echo "      ========================================" 
+echo "      |                                      |" 
+echo "      |     =======               =======    |" 
+echo "      |     =     =               =     =    |" 
+echo "      |     =     =               =     =    |" 
+echo "      |     =     =               =     =    |" 
+echo "      |     =======               =======    |" 
+echo "      ========================================" 
+printf "$DGREY"
+echo "                     |        |               " 
+echo "                     |        |               " 
+printf "$GREY"
+echo "       ======================================" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "       |                                    |" 
+echo "                      Beep Boop              "
+printf "$BLANK" 
+echo ""
+echo ""
